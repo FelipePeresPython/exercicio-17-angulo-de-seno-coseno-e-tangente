04bcbb71a21c79bde425ef51b7edf451721c3a9c
FelipePython <felipehypixel10@gmail.com>
2026-02-02 20:38:16 -0300

    Add files via upload

---
+++ exercicio018.py
@@ -0,0 +1,14 @@
+import math
+
+print("digite o angulo que você deseja")
+L=float(input(""))
+print()
+Sen=math.sin(math.radians(L))
+print(f"o angulo de {L} tem o seno de {Sen:.2f}")
+Cos=math.cos(math.radians(L))
+print()
+print(f"o angulo de {L} tem o coseno de {Cos:.2f}")
+print()
+Tan=math.tan(math.radians(L))
+print(f"o angulo de {L} tem a tangente de {Tan:.2f}")
+print()
